bc4381dc254b86e811650b58cee9bb5e99be26ce
Cash <cashwugeek@icloud.com>
2024-05-29 14:20:29 +0800

    init

---
+++ src/main/java/com/cashwu/javamapstruct/JavaMapStructApplication.java
@@ -0,0 +1,13 @@
+package com.cashwu.javamapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMapStructApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMapStructApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cashwu/javamapstruct/JavaMapStructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashwu.javamapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMapStructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
